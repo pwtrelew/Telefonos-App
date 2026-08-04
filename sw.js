807482f31828ee8edf855e9d9eaa71e83d1ceb5f
trelewapps <bahiarapiman@gmail.com>
2026-08-03 22:35:57 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
-const CACHE_NAME = 'telefonos-app-v7';
+const CACHE_NAME = 'telefonos-app-v8'; // Le pongo v8 para que fuerce la actualización hoy
 const urlsToCache = [
     './',
-    './index.html'
+    './index.html',
+    './icon.png'
 ];
 
 // Instala el Service Worker y guarda en caché los archivos básicos
@@ -13,12 +14,24 @@ self.addEventListener('install', event => {
     self.skipWaiting();
 });
 
-// Activa el Service Worker
+// Activa el Service Worker y LIMPIA LAS CACHÉS VIEJAS (¡La magia que faltaba!)
 self.addEventListener('activate', event => {
-    event.waitUntil(clients.claim());
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    // Si el nombre de la caché no es igual a 'telefonos-app-v8', la borra
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
 });
 
-// Intercepta las solicitudes para funcionar más rápido (y offline en el futuro si se requiere)
+// Intercepta las solicitudes para funcionar más rápido y offline
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
